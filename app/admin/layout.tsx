668291943a593a1aae7dffeca4ed25504edfb79d
benedictleejr <belasolee@gmail.com>
2025-08-24 02:12:02 +0800

    Make home button navigate to landing page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -14,7 +14,7 @@ export default function ProtectedLayout({
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <NavigationText label="Home" path="/admin" />
+            <NavigationText label="Home" path="/" />
             <NavigationText label="Outreach Tool" path="/outreach" />
             <NavigationText label="Package Options" path="/package" />
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
