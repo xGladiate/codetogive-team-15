2b3a16d449fdee64bbd22a48ea4e5c671e4ea239
benedictleejr <belasolee@gmail.com>
2025-08-23 17:11:38 +0800

    Cleanup landing page code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
 import { createClient } from "@/lib/supabase/server";
 import Header from "@/components/landing_page/Header";
 import DataVisualization from "@/components/landing_page/DataVisualization";
@@ -29,33 +21,9 @@ export default async function Home() {
   return (
     <main className="min-h-screen">
       <Header />
-
       <DataVisualization />
-
       <ProgressBar current={totalRaised} goal={campaignGoal} />
-
       <DonationsSection packages={packages || []} />
-    </main>
-  );
-
-  return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-            </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-        </div>
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <ThemeSwitcher />
-        </footer>
-      </div>
     </main>
   );
 }
