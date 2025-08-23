c09677e3dfe900c6049d33919bf55ff773b61179
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-08-23 15:54:58 +0800

    Remove fetch-data-steps.tsx

--- app/donor/page.tsx
+++ app/donor/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -19,16 +18,6 @@ export default async function ProtectedPage() {
           <InfoIcon size="16" strokeWidth={2} />
           This is a donor page that you can only see as a donor. 
         </div>
-      </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(data.claims, null, 2)}
-        </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
