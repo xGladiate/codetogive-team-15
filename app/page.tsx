4e7366dcfe88285f1e6619823b794f0a5a57908f
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-08-23 16:32:59 +0800

    Remove unnecessary elements in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -21,12 +21,7 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
         </div>
-
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <ThemeSwitcher />
         </footer>
