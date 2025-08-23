3eba2704128716d813eba7b68d62db113bcb0288
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-08-24 03:51:43 +0800

    Update routing for donor portal

--- app/donor/layout.tsx
+++ app/donor/layout.tsx
@@ -1,10 +1,3 @@
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
-import NavigationText from "@/components/navigator-text";
-
 export default function ProtectedLayout({
   children,
 }: {
@@ -12,19 +5,9 @@ export default function ProtectedLayout({
 }) {
   return (
     <main className="min-h-screen flex flex-col items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-              <NavigationText label="Donor Dashboard" path="/donor-dashboard" />
-              {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-          </div>
-        </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           {children}
         </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <ThemeSwitcher />
-        </footer>
     </main>
   );
 }
