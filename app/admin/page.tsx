58799cc9d84c2232e4a13a77ec72fb8a2541cac0
xGladiate <125533332+xGladiate@users.noreply.github.com>
2025-08-23 16:33:48 +0800

    refactor: Remove unnecessary elements (#6)
    * Replace profile with users following changes in the database
    
    * Remove fetch-data-steps.tsx
    
    * Remove deploy button
    
    * Remove powered by supabase
    
    * Redirect logout to the main page instead of the login system
    
    * Remove unnecessary elements in the main page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -20,16 +19,6 @@ export default async function ProtectedPage() {
           This is a protected page that you can only see as an authenticated
           user
         </div>
-      </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(data.claims, null, 2)}
-        </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
